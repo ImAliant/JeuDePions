8e63fd6800a81384baea7b3e21e95a89bb60bf8a
diamant <diamant.alexandre@gmail.com>
2021-03-09 19:02:05 +0100

    Vérifié si c'est bon et après on merge

--- src/main/jeu/Plateau.java
+++ src/main/jeu/Plateau.java
@@ -1,16 +1,55 @@
 package main.jeu;
 
+import java.util.Scanner;
+
 public class Plateau {
     private int K; //Alignement requis ( 4 pour Puissance4 | 5 pour Gomoku )
     private int N; //Colonnes du plateau ( 6 pour Puissance4 | 19 pour Gomoku )
     private int M; //Lignes du plateau ( 7 pour Puissance4 | 19 pour Gomuku )
+    private Case[][] cases;
 
-    Plateau(int K, int N, int M){
-        this.K = K;
-        this.N = N;
-        this.M = M;
+    Plateau(){
+        Scanner scanner =new Scanner(System.in);
+
+        // Initialisation du plateau
+        System.out.println("Selection de jeu : Gomoku / Puissance4 / Morpion");
+        if(scanner.nextLine().equals("Gomoku")){
+            K = 5;
+            N = 19;
+            M = 19;
+            cases =new Case[N][M];
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    cases[i][j] =new Case('.');
+                } 
+            }
+        }
+        else if(scanner.nextLine().equals("Puissance4")){
+            K = 4;
+            N = 6;
+            M = 7;
+            cases =new Case[N][M];
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    cases[i][j] =new Case('.');
+                } 
+            }
+        }
+        else if(scanner.nextLine().equals("Morpion")){
+            K = 3;
+            N = 3;
+            M = 3;
+            cases =new Case[N][M];
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    cases[i][j] =new Case('.');
+                } 
+            }
+        }
+        else{
+            System.out.println("Ce jeu n'existe pas !");
+        }
     }
-    Plateau(){}
 
     public int getK() {
         return K;
@@ -34,6 +73,14 @@ public class Plateau {
 
     public void setM(int m) {
         M = m;
+    }
+
+    public Case[][] getCases() {
+        return cases;
+    }
+
+    public void setCases(Case[][] cases) {
+        this.cases = cases;
     }
 
     
