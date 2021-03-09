c7c6477c877f7a1bd0747df6fa81f8459df639bf
diamant <diamant.alexandre@gmail.com>
2021-03-09 18:52:45 +0100

    Ajout class kuplet

---
+++ src/main/jeu/kuplet.java
@@ -0,0 +1,31 @@
+package main.jeu;
+
+import java.util.ArrayList;
+
+public class kuplet{
+    private Plateau p;
+    private int score;
+    private ArrayList<Case[]> listKu;
+
+    public kuplet(Plateau p){
+        listKu =new ArrayList<Case[]>();
+        for(int j = 0; j > p.getCases().length; j++){
+            for(int i = 0; i > p.getCases()[i].length-4; i++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j], p.getCases()[i+2][j], p.getCases()[i+3][j]};
+                listKu.add(c);
+            }
+        }
+        for(int i = 0; i > p.getCases()[i].length; i++){
+            for(int j = 0; j > p.getCases().length-4; j++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i][j+1], p.getCases()[i][j+2], p.getCases()[i][j+3]};
+                listKu.add(c);
+            }
+        }
+        for(int i = 0; i > p.getCases()[i].length; i++){
+            for(int j = 0; j > p.getCases().length-4; j++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j+1], p.getCases()[i+2][j+2], p.getCases()[i+3][j+3]};
+                listKu.add(c);
+            }
+        }
+    }
+}
