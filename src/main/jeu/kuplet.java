83a0ceb7001257ebff418805342ccd4f7f531262
diamant <diamant.alexandre@gmail.com>
2021-03-11 13:00:10 +0100

    modifs

--- src/main/jeu/kuplet.java
+++ src/main/jeu/kuplet.java
@@ -10,20 +10,20 @@ public class kuplet{
     public kuplet(Plateau p){
         listKu =new ArrayList<Case[]>();
         for(int j = 0; j > p.getCases().length; j++){
-            for(int i = 0; i > p.getCases()[i].length-4; i++){
-                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j], p.getCases()[i+2][j], p.getCases()[i+3][j]};
+            for(int i = 0; i > p.getCases()[i].length-5; i++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j], p.getCases()[i+2][j], p.getCases()[i+3][j], p.getCases()[i+4][j]};
                 listKu.add(c);
             }
         }
         for(int i = 0; i > p.getCases()[i].length; i++){
-            for(int j = 0; j > p.getCases().length-4; j++){
-                Case[] c = {p.getCases()[i][j], p.getCases()[i][j+1], p.getCases()[i][j+2], p.getCases()[i][j+3]};
+            for(int j = 0; j > p.getCases().length-5; j++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i][j+1], p.getCases()[i][j+2], p.getCases()[i][j+3], p.getCases()[i][j+4]};
                 listKu.add(c);
             }
         }
         for(int i = 0; i > p.getCases()[i].length; i++){
-            for(int j = 0; j > p.getCases().length-4; j++){
-                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j+1], p.getCases()[i+2][j+2], p.getCases()[i+3][j+3]};
+            for(int j = 0; j > p.getCases().length-5; j++){
+                Case[] c = {p.getCases()[i][j], p.getCases()[i+1][j+1], p.getCases()[i+2][j+2], p.getCases()[i+3][j+3], p.getCases()[i+4][j+4]};
                 listKu.add(c);
             }
         }
