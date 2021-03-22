c470640ce180547576c9c879de4e82a6ca26eaf2
Aymen Abid <aymen@MacBook-Pro-de-Aymen.local>
2021-03-22 22:03:55 +0100

    Changement Interdependance

--- src/main/jeu/Case.java
+++ src/main/jeu/Case.java
@@ -3,28 +3,42 @@ package main.jeu;
 import java.util.ArrayList;
 
 public class Case {
-    private char etat;
-    private ArrayList li;
-    private int i;
-    private int j;
+    private Plateau p;
+    private int etat;
+    private ArrayList<kuplet> li;
+    private int x;
+    private int y;
 
-    Case(char etat){
+    Case(int etat, int x, int y, Plateau p){
+        this.x = x;
+        this.y = y;
         this.etat = etat;
+        li =new ArrayList<kuplet>();
+        kuplet k =new kuplet(x, y, p);
+        li.add(k); 
     }
+
+    
 
     public char getEtat() {
-        return etat;
+        if(etat == 1){
+            return 'X';
+        }
+        if(etat == 2){
+            return 'O';
+        }
+        else return '.';
     }
 
-    public void setEtat(char etat) {
+    public void setEtat(int etat) {
         this.etat = etat;
     }
 
-    public kuplet getLi() {
+    public ArrayList<kuplet> getLi() {
         return li;
     }
 
-    public void setLi(kuplet li) {
+    public void setLi(ArrayList<kuplet> li) {
         this.li = li;
     }
 
