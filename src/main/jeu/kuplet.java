5ff31bd351dafcea85afd43c93a5fdf04da52373
Essahlaoui Mouadh <moud200@hotmail.fr>
2021-04-16 01:33:39 +0200

    score

--- src/main/jeu/kuplet.java
+++ src/main/jeu/kuplet.java
@@ -7,52 +7,8 @@ public class kuplet{
 
     public kuplet(Plateau p, Case[] c){
         this.p = p;
-        this.c = c;
-        switch(c.length) {
-        case 5:
-        	if(countEtat('X')==c.length-1 && alignement('X', 4) ||countEtat('O')==c.length-1 && alignement('O', 4)) {
-            	score=4;
-            }
-            else if(countEtat('X')==c.length-2 && alignement('X', 3)||countEtat('O')==c.length-2 && alignement('O', 3)) {
-            	score=3;
-            }
-            else if(countEtat('X')==c.length-3 && alignement('X', 2) ||countEtat('O')==c.length-3 && alignement('O', 2)) {
-            	score=2;
-            }
-            else if(countEtat('X')==c.length-4 ||countEtat('O')==c.length-4) {
-            	score=1;
-            }
-            else if(countEtat('X')>=1 && countEtat('O')>=1){
-            	score=0;
-            }
-        	break;
-        case 4:
-        	if(countEtat('X')==c.length-1 && alignement('X', 3)||countEtat('O')==c.length-1&& alignement('O', 3)) {
-            	score=3;
-            }
-            else if(countEtat('X')==c.length-2 && alignement('X', 2)||countEtat('O')==c.length-2 && alignement('O', 2)) {
-            	score=2;
-            }
-            else if(countEtat('X')==c.length-3 ||countEtat('O')==c.length-3) {
-            	score=1;
-            }
-            else if(countEtat('X')>=1 && countEtat('O')>=1){
-            	score=0;
-            }
-        	break;
-        case 3:
-        	if(countEtat('X')==c.length-1 && alignement('X', 2)||countEtat('O')==c.length-1 && alignement('O', 2)) {
-            	score=2;
-            }
-            else if(countEtat('X')==c.length-2 ||countEtat('O')==c.length-2) {
-            	score=1;
-            }
-            else if(countEtat('X')>=1 && countEtat('O')>=1){
-            	score=0;
-            }
-        	break;
-        }
-       }
+        this.c = c;        
+    }
 
     public Plateau getP() {
         return p;
@@ -88,21 +44,35 @@ public class kuplet{
     	return cpt;
     }
     
-    public boolean alignement(char etat, int nbr) {
-    	int x=0;
-    	for(int i=0;i<c.length;i++) {
-    		if(c[i].getEtat()==etat) {
-    			x=i;
-    			for(int j=i+1;j<nbr;j++) {
-    				if(c[j].getEtat()!=etat) {
-    					return false;
-    				}
-    			}
+    public int score() {
+    	int x=countEtat('X');
+    	int y=countEtat('O');
+    	if(x>=1 && y>=1 || x==0 && y==0) {
+    		score=0;
+        }
+    	switch(x) {
+    	case 0:
+    		if(y==1) {
+    			score=5;
     		}
+    		else if(y==2) {
+    			score=10;
+    		}
+    		else if(y==3) {
+    			score=15;
+    		}
+    		else if(y==4) {
+    			score=20;
+    		}
+    	case 1:
+    		score=3;
+    	case 2:
+    		score=8;
+    	case 3:
+    		score=13;
+    	case 4:
+    		score=18;
     	}
-    	if(x>0 && c[x].getEtat()=='.' || nbr<c.length && c[nbr].getEtat()=='.') {
-    		return true;
-    	}
-    	return false;
+    	return score;
     }
 }
