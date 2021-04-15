f7f480a9ae3d7bc4c519d7cd37bf917eb07bf028
Aymen <abidaymen75@gmail.com>
2021-04-15 19:05:01 +0200

    LettreTrad

--- src/main/jeu/Jeu.java
+++ src/main/jeu/Jeu.java
@@ -145,7 +145,7 @@ public class Jeu {
                 System.out.println("Entrez le numéro de la colonne entre 1 et " + p.getN() + " ...");
                 String lC =scanner.nextLine();
                 try{
-                    col = Integer.valueOf(lC);
+                    col = lettreVersChiffre(lC);
 
                     if(col >= 1 && col <= p.getN()){
                         System.out.println("Entrez le numéro de la ligne entre 1 et " + p.getM() + " ...");
@@ -285,5 +285,66 @@ public class Jeu {
             System.out.println("****FIN DE PARTIE****");
 			System.out.println("****VICTOIRE DE O****");
         }
+    }
+    public int lettreVersChiffre(String s){
+        int col = 0;
+        if(s.equals("A")||s.equals("a")){
+            col = 1;            
+        } 
+        else if(s.equals("B")||s.equals("b")){
+            col = 2;         
+        } 
+        else if(s.equals("C")||s.equals("c")){
+            col = 3;           
+        } 
+        else if(s.equals("D")||s.equals("d")){
+            col = 4;           
+        } 
+        else if(s.equals("E")||s.equals("e")){
+            col = 5;           
+        } 
+        else if(s.equals("F")||s.equals("f")){
+            col = 6;           
+        } 
+        else if(s.equals("G")||s.equals("g")){
+            col = 7;           
+        } 
+        else if(s.equals("H")||s.equals("h")){
+            col = 8;           
+        } 
+        else if(s.equals("I")||s.equals("i")){
+            col = 9;            
+        } 
+        else if(s.equals("J")||s.equals("j")){
+            col = 10;           
+        } 
+        else if(s.equals("K")||s.equals("k")){
+            col = 11;            
+        } 
+        else if(s.equals("L")||s.equals("l")){
+            col = 12;            
+        } 
+        else if(s.equals("M")||s.equals("m")){
+            col = 13;           
+        } 
+        else if(s.equals("N")||s.equals("n")){
+            col = 14;           
+        } 
+        else if(s.equals("O")||s.equals("o")){
+            col = 15;            
+        } 
+        else if(s.equals("P")||s.equals("p")){
+            col = 16;            
+        } 
+        else if(s.equals("Q")||s.equals("q")){
+            col = 17;            
+        } 
+        else if(s.equals("R")||s.equals("r")){
+            col = 18;           
+        } 
+        else if(s.equals("S")||s.equals("s")){
+            col = 19;            
+        } 
+        return col;
     }
 }
